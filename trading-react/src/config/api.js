9db3a0051344d1c8a15bdc6596436dab62cfc410
MohamedYashar <arrafathyas@gmail.com>
2026-01-10 18:46:38 +0530

    change local host to railway cloud server

--- trading-react/src/config/api.js
+++ trading-react/src/config/api.js
@@ -3,6 +3,8 @@ import axios from "axios";
 // export const API_BASE_URL ="http://localhost:8088";
 
 export const API_BASE_URL ="https://crypto-server-springboot-production.up.railway.app";
+
+// https://crypto-server-springboot-production.up.railway.app/
 // crypto-server-springboot-production.up.railway.app
 
 const api = axios.create({
